fe85d1894ce1a0dda6bc44a220ec8bd34efbe615
Hajeevan26 <50508738+Hajeevan26@users.noreply.github.com>
2020-09-01 16:53:36 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@ let button = document.getElementsByTagName('button')[0];
 let content = document.getElementById('content');
 button.addEventListener("click", function () {
     content.innerHTML = "<p> double click</p>";
+    content.innerHTML = "<p> double click</p>";
 });
 console.log(button);
