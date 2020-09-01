6bf1fb26cb43aa1943d4e2d8df3a29d56f51dded
Hajeevan <hajeevan26@gmail.com>
2020-09-01 16:29:16 +0530

    adding p

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let button = document.getElementsByTagName('button')[0];
 let content = document.getElementById('content');
 button.addEventListener("click", function () {
-    content.innerHTML = "hello";
+    content.innerHTML = "<p> devops lisbro</p>";
 });
 console.log(button);
